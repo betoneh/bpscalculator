8ca71cbd717d3bb144da16596d472d4faf351755
iamnmddd <iamnehmad@gmail.com>
2026-04-30 22:09:56 -0600

    Fix receipt PDF logo and add PWA icons

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ const MIME_TYPES = {
   '.html': 'text/html; charset=utf-8',
   '.js': 'text/javascript; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
+  '.webmanifest': 'application/manifest+json; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
   '.png': 'image/png',
   '.svg': 'image/svg+xml; charset=utf-8',
+  '.ico': 'image/x-icon',
   '.md': 'text/markdown; charset=utf-8',
   '.gs': 'text/plain; charset=utf-8'
 };
